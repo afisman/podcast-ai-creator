e7fb4ca83d192112fcd13d5a90a124b8f0429076
afisman <afisman@gmail.com>
2024-07-30 14:36:40 +0200

    worked on left sidebar, added constants

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const PodcastDetails = ({ params }: { params: { podcastId: string } }) => {
-    console.log(params.podcastId)
     return (
         <p className='text-white-1'>
             Podcast Details for {params.podcastId}
