ee09016c1ff5bd02958be00c32f0adf3e8f7dea3
afisman <afisman@gmail.com>
2024-07-30 19:40:30 +0200

    worked on podcastcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,17 +9,18 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
-        <Button className='text-white-1 bg-orange-1'>Button</Button>
-        {podcastData.map(({ id, title, description, imgURL }) => (
-          <PodcastCard
-            key={id}
-            imgUrl={imgURL}
-            title={title}
-            description={description}
-            podcastId={id}
+        <div className='podcast_grid'>
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard
+              key={id}
+              imgUrl={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
 
-          />
-        ))}
+            />
+          ))}
+        </div>
       </section>
     </div>
   )
