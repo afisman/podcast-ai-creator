f5f4f0c16f581607a16fd5452eb88b4424f2e028
afisman <afisman@gmail.com>
2024-07-31 15:42:09 +0200

    began clerk integration

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://assuring-oryx-10.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
