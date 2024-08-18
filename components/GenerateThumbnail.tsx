7d2839505d1dcefdcd7d0b890d33e7e219deaa84
afisman <afisman@gmail.com>
2024-08-18 13:33:29 +0200

    started om generating thumbnail

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+import { Label } from './ui/label';
+import { Textarea } from './ui/textarea';
+import { GenerateThumbnailProps } from '@/types';
+import { Loader } from 'lucide-react';
 
-const GenerateThumbnail = () => {
+
+
+
+const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, setImagePrompt }: GenerateThumbnailProps) => {
+    const [isAIThumbnail, setIsAIThumbnail] = useState(true);
+    const [isGenerating, setIsGenerating] = useState(false)
+
+    const generateImage = async () => {
+
+    }
+
     return (
-        <div>GenerateThumbnail</div>
+        <>
+            <div className='generate_thumbnail'>
+                <Button
+                    type='button'
+                    variant='plain'
+                    onClick={() => setIsAIThumbnail(true)}
+                    className={cn('', {
+                        'bg-blac-6': isAIThumbnail
+                    })}
+                >
+                    Use AI to generate thumbnail
+                </Button>
+                <Button
+                    type='button'
+                    variant='plain'
+                    onClick={() => setIsAIThumbnail(false)}
+                    className={cn('', {
+                        'bg-blac-6': !isAIThumbnail
+                    })}
+                >
+                    Upload custom image
+                </Button>
+            </div>
+            {
+                isAIThumbnail ? (
+                    <div>
+                        <div className='flex flex-col gap-2.5'>
+                            <Label className='text-16 font-bold text-white-1'>
+                                AI prompt to generate podcast
+                            </Label>
+                            <Textarea
+                                className='input-class font-light focus-visible:ring-offset-orange-1'
+                                placeholder='Provide text to generate audio'
+                                rows={5}
+                                value={imagePrompt}
+                                onChange={(e) => setImagePrompt(e.target.value)}
+                            />
+                        </div>
+                        <div className='mt-5 w-full max-w-[200px]'>
+                            <Button type="submit"
+                                className="text-16 bg-orange-1 py-4 font-bold text-white-1"
+                                onClick={generateImage}
+                            >
+                                {isGenerating ? (
+                                    <>
+                                        Generating
+                                        <Loader size={20} className="animate-spin ml-2" />
+                                    </>
+                                )
+                                    :
+                                    "Generate podcast"
+                                }
+
+                            </Button>
+                        </div>
+                    </div>
+
+                ) : (
+                    <div>
+
+                    </div>
+                )
+            }
+        </>
     )
 }
 
